a9d25989163da677d5aa2cf0498c099eb8e7ead8
ZhuChuanhai <zhuchuanhai@airbears2-10-142-65-116.airbears2.1918.berkeley.edu>
2016-02-01 00:54:28 -0800

    make it better b

--- stock_app.py
+++ stock_app.py
@@ -1,16 +1,17 @@
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template
 app_stock = Flask(__name__)
 
 @app_stock.route('/main')
 def hello_stock():
-	return render_template('Main.html')
-@app_stock.route('/Stock_Open_Price',methods=['POST'])
-def Stock_Open_Price():
-	return render_template('Stock_Open_Price.html')
+	return "ok"
+# 	return render_template('Main.html')
+# @app_stock.route('/Stock_Open_Price',methods=['POST'])
+# def Stock_Open_Price():
+# 	return render_template('Stock_Open_Price.html')
 
-@app_stock.route('/ROI',methods=['POST'])	
-def ROI():
-	return render_template('ROI.html')	
+# @app_stock.route('/ROI',methods=['POST'])	
+# def ROI():
+# 	return render_template('ROI.html')	
 
 if __name__ == '__main__':
     app_stock.run(debug=True)

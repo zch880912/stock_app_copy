a6ec9beb21371c8ca764457244378528c74233bf
ZhuChuanhai <zhuchuanhai@airbears2-10-142-65-116.airbears2.1918.berkeley.edu>
2016-01-31 23:51:12 -0800

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn stock_app.wsgi --log-file -
---
+++ stock_app.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template,request,redirect
+app_stock = Flask(__name__)
+
+@app_stock.route('/main')
+def hello_stock():
+	return render_template('Main.html')
+@app_stock.route('/Stock_Open_Price',methods=['POST'])
+def Stock_Open_Price():
+	return render_template('Stock_Open_Price.html')
+
+@app_stock.route('/ROI',methods=['POST'])	
+def ROI():
+	return render_template('ROI.html')	
+
+if __name__ == '__main__':
+    app_stock.run(debug=True)
